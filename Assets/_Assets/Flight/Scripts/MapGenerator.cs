3fe03c4a48ce7aaa63e7c0c9f7036099ee9375f9
Brandon Coffey <bmc180001@utdallas.edu>
2021-10-25 16:07:38 -0500

    Reorganize project. Add flight and tabletop cars.

---
+++ Assets/_Assets/Flight/Scripts/MapGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public int viewDistance = 600;
+    public int seperation = 30;
+    public GameObject player;
+    public List<GameObject> buildings;
+    FastNoise mapNoise = new FastNoise();
+    GameObject map;
+
+    void Update()
+    {
+        DestroyMap();
+        LoadMap(Mathf.FloorToInt((player.transform.position.x + player.transform.forward.x * viewDistance / 1.5f) / seperation) * seperation,
+        Mathf.FloorToInt((player.transform.position.z + player.transform.forward.z * viewDistance / 1.5f) / seperation) * seperation);
+    }
+
+    void DestroyMap()
+    {
+        Destroy(map);
+    }
+
+    void LoadMap(int px, int pz)
+    {
+        map = new GameObject();
+        for (int x = px - viewDistance; x <= px + viewDistance; x += seperation)
+        {
+            for (int z = pz - viewDistance; z <= pz + viewDistance; z += seperation)
+            {
+                float heightMap = mapNoise.GetSimplex((x) * 4f, (z) * 4f);
+                if (heightMap > 0.2f && heightMap < 0.3f)
+                {
+                    GameObject newBuilding = Instantiate(buildings[0], new Vector3(x, heightMap * 20f, z), Quaternion.identity);
+                    newBuilding.transform.parent = map.transform;
+                }
+                else if (heightMap > -0.4f && heightMap < -0.3f)
+                {
+                    GameObject newBuilding = Instantiate(buildings[1], new Vector3(x, heightMap * 20f, z), Quaternion.identity);
+                    newBuilding.transform.parent = map.transform;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/_Assets/Flight/Scripts/PlaneControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneControl : MonoBehaviour
+{
+    public int speedMultiplier;
+    public float speed;
+    public float minSpeed;
+    public float maxSpeed;
+    public GameObject camera;
+    public float cameraBias;
+
+    void FixedUpdate()
+    {
+        Vector3 moveCam = transform.position - transform.forward * 12f + transform.up * 10.0f;
+        camera.transform.position = camera.transform.position * cameraBias + moveCam * (1 - cameraBias);
+        camera.transform.LookAt(transform.position);
+
+        transform.position += transform.forward * Time.deltaTime * speed;
+        speed -= transform.forward.y * Time.deltaTime * speedMultiplier;
+        if (speed < minSpeed)
+        {
+            speed = minSpeed;
+        }
+        else if (speed > maxSpeed)
+        {
+            speed = maxSpeed;
+        }
+        transform.Rotate(Input.GetAxis("Vertical"), 0.0f, -Input.GetAxis("Horizontal"));
+    }
+}
